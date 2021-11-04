db685d18c68a7eeb54c17eb7115e0db4109ff427
Повелитель <874967586@bk.ru>
2021-11-04 20:05:02 +0400

    lesson2: first step

--- src/main/java/FirstProgram.java
+++ src/main/java/FirstProgram.java
@@ -1,5 +1,11 @@
 public class FirstProgram {
     public static void main(String[] args) {
         System.out.println("Привет, мир!");
+        long summa = calculate(7, 2, 4);
+        System.out.println(summa);
+    }
+    public static long calculate(int arg1, int arg2, int arg3) {
+        long result = (arg1 + arg2) - arg3;
+        return result;
     }
 }
